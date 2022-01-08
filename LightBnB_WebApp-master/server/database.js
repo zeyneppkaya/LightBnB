84c719ab7ea620a235e8e29b74b3089537adbfa0
Zeynep <zeynepkaya_26@hotmail.com>
2022-01-08 00:06:04 +0000

    Implementing the getAllReservations function to use the lightbnb database

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -69,7 +69,6 @@ const addUser =  function(user) {
     RETURNING *;
     `, [user.name, user.email, user.password])
     .then(res => {
-      console.log(res.rows);
       return res.rows[0];
     })
     .catch(err => {
@@ -87,7 +86,22 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool.query(`
+  SELECT reservations.*, properties.*,  AVG(rating) AS average_rating
+  FROM properties
+  JOIN reservations ON properties.id = reservations.property_id
+  JOIN property_reviews ON properties.id = property_reviews.property_id
+  WHERE reservations.guest_id = $1 AND end_date < now()::date
+  GROUP BY properties.id, reservations.id
+  ORDER BY start_date
+  LIMIT $2;
+  `, [guest_id, limit])
+  .then(res => {
+    return res.rows;
+  })
+  .catch(err => {
+    return null;
+  });
 }
 exports.getAllReservations = getAllReservations;
 
